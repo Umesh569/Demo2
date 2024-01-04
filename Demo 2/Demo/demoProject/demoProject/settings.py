274b9733523bb27729c1b9f731ab42b620d1e325
root <root@ip-172-31-20-7.ec2.internal>
2024-01-04 14:01:26 +0000

    Allowed all host

--- Demo 2/Demo/demoProject/demoProject/settings.py
+++ Demo 2/Demo/demoProject/demoProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-srfy9)7pgtlepbd4^@0l_n45a!8u)afp5dhdp*qw--u1y_9xs6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
